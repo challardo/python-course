4f47a7525ecac50b11afe95e0f7a2804336af35d
Carlos <carlosforjadores@gmail.com>
2024-05-21 07:54:14 -0700

    added url and topic variable to url

--- email-digest/main.py
+++ email-digest/main.py
@@ -5,16 +5,27 @@ from send_email import send_email
 
 load_dotenv()
 
+topic = "tesla"
+
 weather_api_key = os.getenv("WEATHER_API_KEY")
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-04-20&sortBy=publishedAt&apiKey={weather_api_key}"
+url = f"https://newsapi.org/v2/everything?q={topic}&sortBy=publishedAt&apiKey={weather_api_key}&language=en"
 
 req = requests.get(url)
 content = req.json()
 
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body += str(article["title"]) + "\n" + str(article["description"]) + "\n\n"
+        body += (
+            "Subject: Today's news "
+            + "\n"
+            + str(article["title"])
+            + "\n"
+            + str(article["description"])
+            + "\n"
+            + str(article["url"])
+            + "\n\n"
+        )
 
 body = body.encode("utf-8")
 send_email(message=body)
