22334c212e30a6cc06844605514a432ca77aab14
Carlos Gallardo <83609061+challardo@users.noreply.github.com>
2024-05-23 08:53:53 -0700

    fix route

--- weather-api/main.py
+++ weather-api/main.py
@@ -9,7 +9,7 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/api/v1/<stations>/<date>")
+@app.route("/api/v1/<station>/<date>")
 def get_temperature(station, date):
     filename = "./data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
