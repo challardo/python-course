ad4daf145ef8960ffbe7c9fd27b09748aaeda843
Carlos <carlosforjadores@gmail.com>
2024-05-30 09:04:52 -0700

    added plot

--- weather-forecast/main.py
+++ weather-forecast/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for the next days")
 
@@ -9,3 +10,17 @@ days = st.slider(
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} of the next {days} days in {place}")
+
+
+def get_date(days):
+    dates = ["2022", "2023", "2024"]
+    temperatures = [0, 10, 30]
+    temperatures = [days * i for i in temperatures]
+
+    return dates, temperatures
+
+
+d, t = get_date(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
