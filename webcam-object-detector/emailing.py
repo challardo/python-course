5b4adc54814e49666538ffc29e2b584f13856b85
Carlos <carlosforjadores@gmail.com>
2024-06-14 07:57:32 -0700

    send email function on object recognition

--- webcam-object-detector/emailing.py
+++ webcam-object-detector/emailing.py
@@ -1,2 +1,36 @@
-def send_email():
-    print("email sent")
+import smtplib, ssl
+import os
+import imghdr
+from dotenv import load_dotenv
+from email.message import EmailMessage
+
+load_dotenv()
+
+
+def send_email(image_path):
+    host = os.getenv("EMAIL_HOST")
+    port = os.getenv("EMAIL_PORT")
+    username = os.getenv("EMAIL_USER")
+    password = os.getenv("EMAIL_PASSWORD")
+
+    receiver = os.getenv("EMAIL_RECIEVER")
+    context = ssl.create_default_context()
+
+    email_message = EmailMessage()
+    email_message["subject"] = "Subject showd up!"
+    email_message.set_content("we saw a new subject")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(
+        content, maintype="image", subtype=imghdr.what(None, content)
+    )
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, email_message.as_string())
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/21.png")
--- webcam-object-detector/main.py
+++ webcam-object-detector/main.py
@@ -1,5 +1,8 @@
 import cv2
 import time
+import os
+import glob
+from threading import Thread
 from emailing import send_email
 
 video = cv2.VideoCapture(0)
@@ -7,9 +10,23 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+
+def clean_folder():
+    print("clean folder function")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
+
 while True:
     status = 0
     check, frame = video.read()
+
+    clean_folder_thread = Thread(target=clean_folder)
+    clean_folder_thread.daemon = True
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -34,12 +51,19 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(image_with_object,))
+        email_thread.daemon = True
+        email_thread.start()
 
     cv2.imshow("my video", frame)
     key = cv2.waitKey(1)
@@ -47,4 +71,5 @@ while True:
     if key == ord("q"):
         break
 
+clean_folder_thread.start()
 video.release()
