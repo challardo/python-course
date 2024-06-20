effdb45626928e5e345ebc4b3ea3026824ae0a35
Carlos <carlosforjadores@gmail.com>
2024-06-20 07:44:50 -0700

    credit card class

--- hotel-booking/main.py
+++ hotel-booking/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -35,13 +36,35 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {
+            "number": self.number,
+            "expiration": expiration,
+            "holder": holder,
+            "cvc": cvc,
+        }
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
 print(df)
 hotel_id = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_id)
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+    card_number = input("Enter credit card number")
+    credit_card = CreditCard(number=1636434563212432)
+    if credit_card.validate(expiration="12/31", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("there is a problem with your payment")
 else:
     print("hotel is not available")
