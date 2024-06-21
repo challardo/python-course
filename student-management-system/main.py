0187459c5db23f9485e2b8b6105260a9076c241a
Carlos <carlosforjadores@gmail.com>
2024-06-21 08:49:27 -0700

    added database and table data

--- student-management-system/main.py
+++ student-management-system/main.py
@@ -8,9 +8,11 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QMainWindow,
     QTableWidget,
+    QTableWidgetItem,
 )
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -31,13 +33,25 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        # self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def load_data(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(
+                    row_number, column_number, QTableWidgetItem(str(data))
+                )
+        connection.close()
 
 
 app = QApplication(sys.argv)
 mainApp = MainWindow()
 mainApp.show()
+mainApp.load_data()
 sys.exit(app.exec())
