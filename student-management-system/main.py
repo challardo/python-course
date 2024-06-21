13051edbf1d9c5276ffaf1a142a5f6ab86e174f8
Carlos <carlosforjadores@gmail.com>
2024-06-21 08:32:07 -0700

    initial main window

---
+++ student-management-system/main.py
@@ -0,0 +1,34 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QWidget,
+    QGridLayout,
+    QLineEdit,
+    QPushButton,
+    QMainWindow,
+)
+from PyQt6.QtGui import QAction
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
+
+
+app = QApplication(sys.argv)
+mainApp = MainWindow()
+mainApp.show()
+sys.exit(app.exec())
