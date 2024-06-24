598d79708fbda50b6c1c87a2677ba5d28492ee7f
Carlos <carlosforjadores@gmail.com>
2024-06-23 20:23:01 -0700

    about dialog

--- student-management-system/main.py
+++ student-management-system/main.py
@@ -38,7 +38,7 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
-
+        about_action.triggered.connect(self.about)
         search_action = QAction(QIcon("./icons/search.png"), "Search", self)
         search_action.triggered.connect(self.search)
         edit_menu_item.addAction(search_action)
@@ -100,6 +100,10 @@ class MainWindow(QMainWindow):
 
     def delete(self):
         dialog = DeleteDialog()
+        dialog.exec()
+
+    def about(self):
+        dialog = AboutDialog()
         dialog.exec()
 
 
@@ -273,6 +277,14 @@ class DeleteDialog(QDialog):
         confirmation_widget.setWindowTitle("Deleted student")
         confirmation_widget.setText("Successfully deleted.")
         confirmation_widget.exec()
+
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About")
+        content = """this is the about page"""
+        self.setText(content)
 
 
 app = QApplication(sys.argv)
