f2dcde4a883677f4e428f31318f61b783f4a28c4
Carlos <carlosforjadores@gmail.com>
2024-07-02 07:59:39 -0700

    context data

--- restaurant-menu-django/restaurant_menu/views.py
+++ restaurant-menu-django/restaurant_menu/views.py
@@ -8,6 +8,10 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {"meals": ["Pizza", "pasta"], "ingrediants": ["something"]}
+        return context
+
 
 class MenuItemDetail(generic.DetailView):
     model = Item
